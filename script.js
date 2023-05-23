dc4946df761c378b0e5276808bd70b6b460ef41f
Ahsan Abbasi <1abbasia@gmail.com>
2023-05-23 12:43:58 -0400

    added comments for guidance in js code

---
+++ script.js
@@ -0,0 +1,48 @@
+// Generate a random number between 1 and 100
+
+
+// Get DOM elements
+
+//Declare empty attempts variable ( set it to 0)
+
+
+// Function to check the user's guess
+function checkGuess() {
+  // Get the user's guess from the input field
+  
+
+  // Validate if the input is a number
+  
+
+  // Increase the number of attempts
+ 
+
+  // Check if the guess is correct
+  
+    // Disable the input and guess button
+    
+
+  // Update the attempt count
+  
+}
+
+// Function to restart the game
+function restartGame() {
+  // Reset the attempts count
+  
+
+  // Reset the input field and feedback message
+  
+
+  // Enable the input and guess button
+  
+
+  // Generate a new random number
+  
+}
+
+// Event listener for the guess button
+
+
+// Event listener for the restart button
+
